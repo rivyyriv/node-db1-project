4d47503016f6291212df0ff67653abec893699b9
River <rivyyriv@gmail.com>
2021-04-21 19:13:31 -0600

    MVP, DB funtioncs. CRUDS

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const server = require("./api/server.js");
+
+const express = require('express');
+
+const server = express();
+
+const accountRoutes = require("./api/accounts/accounts-router.js");
+
+server.use('/api/accounts', accountRoutes);
 
 const PORT = process.env.PORT || 5000;
 
